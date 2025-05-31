ba9e15c296460eb8c189520f2ab63065fdaca80d
r2hu1 <hi@rahul.eu.org>
2025-05-31 14:00:08 +0530

    feat: db init

---
+++ server/utils/db.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+const { connect } = mongoose;
+import dotenv from "dotenv";
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MongoDB connection URI is not defined");
+    }
+    await connect(process.env.MONGO_URI);
+  } catch (e) {
+    console.error("MongoDB connection error:", e);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
