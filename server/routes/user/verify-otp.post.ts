979cb0767a4fef3b3103ae119d796acd32cfc931
r2hu1 <hi@rahul.eu.org>
2025-05-31 14:00:47 +0530

    feat: verify otp

---
+++ server/routes/user/verify-otp.post.ts
@@ -0,0 +1,35 @@
+import { Model } from "mongoose";
+import { IOtpModel, Otp } from "~/models/otp";
+import User, { IUser } from "~/models/user";
+import connectDB from "~/utils/db";
+
+export default defineEventHandler(async (event) => {
+  await connectDB();
+  const { otp } = await readBody(event);
+  const { id } = event.context.user;
+
+  const record = await (Otp as Model<IOtpModel>).findOne({ userId: id });
+
+  if (!record) {
+    return createError({ statusCode: 404, statusMessage: "OTP not found" });
+  }
+
+  if (record.expiresAt < new Date()) {
+    await Otp.deleteOne({ id });
+    return createError({ statusCode: 400, statusMessage: "OTP expired" });
+  }
+
+  const isMatch = otp === decrypt(record.otpHash);
+  if (!isMatch) {
+    return createError({ statusCode: 400, statusMessage: "Invalid OTP" });
+  }
+
+  await (Otp as Model<IOtpModel>).deleteMany({ userId: id });
+  await (User as Model<IUser>).updateOne({ _id: id }, { status: "verified" });
+  return {
+    success: true,
+    error: false,
+    statusCode: 200,
+    message: "OTP verified successfully",
+  };
+});
