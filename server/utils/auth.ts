cd0f563dc1c48f39120631e5b50287e8fb86c012
r2hu1 <hi@rahul.eu.org>
2025-06-11 21:47:13 +0530

    feat: aes-256-gcm encryption method

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,13 +1,10 @@
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-
-dotenv.config();
+import bcrypt from "bcryptjs";
 
 const SECRET = process.env.JWT_SECRET;
 
 export function generateEncryptedToken(payload: object): string {
-  const token = jwt.sign(payload, SECRET, { expiresIn: "1h" });
+  const token = jwt.sign(payload, SECRET, { expiresIn: "24h" });
   return encrypt(token);
 }
 
@@ -20,13 +17,9 @@ export function verifyEncryptedToken(encryptedToken: string): any | null {
   }
 }
 
-export async function hashPassword(password: string): Promise<string> {
-  return await bcrypt.hash(password, 10);
-}
-
 export async function verifyPassword(
   password: string,
   hash: string,
 ): Promise<boolean> {
-  return password == decrypt(hash);
+  return await bcrypt.compare(password, hash);
 }
